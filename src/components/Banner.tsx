e0ece96278014c5965da519f6db90ae60b49f6f3
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-12-12 22:42:41 +0700

    init autoplay card

---
+++ src/components/Banner.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import SimpleSlider from "./BannerSlide";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+export default function Banner() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+
+      <div className="w-full h-50vh items-center text-center">
+        <SimpleSlider />
+      </div>
+    </>
+  );
+}
---
+++ src/components/BannerSlide.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import BannerCardSlider from "./BannerCardSlider";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      speed: 3000,
+      autoplaySpeed: 500,
+      cssEase: "linear",
+    };
+    return (
+      <div className="mx-auto p-10 w-80 text-gray-800 w-[80%] space-x-10">
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <BannerCardSlider />
+          <BannerCardSlider />
+          <BannerCardSlider />
+          <BannerCardSlider />
+        </Slider>
+      </div>
+    );
+  }
+}
