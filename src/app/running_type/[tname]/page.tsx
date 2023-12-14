0fdd1aa2b067763f3edbd22eac2b3e4ac6a1c65d
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-12-14 19:37:46 +0700

    fix minor nav bar

--- src/app/running_type/[tname]/page.tsx
+++ src/app/running_type/[tname]/page.tsx
@@ -1,6 +1,6 @@
 export default function TypePage({ params }: { params: { tname: string } }) {
   return (
-    <main className="bg-neutral-100 w-full h-[400vh] flex flex-col items-center pt-[70px]">
+    <main className="bg-neutral-100 w-full h-[400vh] flex flex-col items-center pt-[120px]">
       <div className="text-black">{params.tname}</div>
     </main>
   );
--- src/app/running_type/page.tsx
+++ src/app/running_type/page.tsx
@@ -1,5 +1,7 @@
 export default function TypeHome() {
   return (
-    <main className="bg-neutral-100 w-full h-[400vh] flex flex-col items-center pt-[70px]"></main>
+    <main className="bg-neutral-100 w-full h-[400vh] flex flex-col items-center pt-[120px] text-black">
+      TEST
+    </main>
   );
 }
