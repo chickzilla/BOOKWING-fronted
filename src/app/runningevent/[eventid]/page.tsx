09591f8abfd673c3aece629b82de11d8520fffee
ReawEiEi <chot.fueng@gmail.com>
2023-12-14 21:11:14 +0700

    add event detail page

---
+++ src/app/runningevent/[eventid]/page.tsx
@@ -0,0 +1,7 @@
+export default function EventDatailPage() {
+  return (
+    <main className="bg-neutral-100 w-full h-[400vh] flex flex-col items-center pt-[120px]">
+      <div className="text-black">Event Detail Page</div>
+    </main>
+  );
+}
--- src/components/Closing/ClosingItem.tsx
+++ src/components/Closing/ClosingItem.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import Link from "next/link";
 
 export default function ClosingItem() {
   return (
-    <div className="flex flex-row items-center bg-white w-[100%] h-[33%] justify-between px-5 rounded-xl shadow-md border-2 border-black hover:cursor-pointer duration-300 hover:shadow-lg hover:scale-105">
+    <Link
+      href="/runningevent/eventid"
+      className="flex flex-row items-center bg-white w-[100%] h-[33%] justify-between px-5 rounded-xl shadow-md border-2 border-black hover:cursor-pointer duration-300 hover:shadow-lg hover:scale-105"
+    >
       <div className=" flex flex-col items-start justify-between space-y-2">
         <div className="text-xl text-black font-semibold text-start my-2">
           Rayong Marathon
@@ -29,6 +33,6 @@ export default function ClosingItem() {
         width={100}
         height={100}
       />
-    </div>
+    </Link>
   );
 }
