cd76328b72f75450cf41320c9540a6d0ea6cd0cb
ReawEiEi <chot.fueng@gmail.com>
2023-12-14 17:06:27 +0700

    update link to main page

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,12 +1,23 @@
 import RunCircleIcon from "@mui/icons-material/RunCircle";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="bg-[#B22222] fixed w-full h-[70px] z-20 top-0 start-0 flex flex-row items-center">
-      <div className="h-full w-[10%] text-center flex items-center font-bold text-xl p-2">
-        <RunCircleIcon sx={{ color: "white", width: "60%", height: "70%" }} />
-        BookWing
-      </div>
+      <Link href="../">
+        <div className="h-full w-[22%] text-center flex items-center font-bold text-xl p-2">
+          <RunCircleIcon
+            sx={{
+              color: "white",
+              width: "60%",
+              height: "70%",
+              marginRight: "10px",
+              marginLeft: "8px",
+            }}
+          />
+          BookWing
+        </div>
+      </Link>
     </nav>
   );
 }
