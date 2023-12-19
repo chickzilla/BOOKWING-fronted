a47181e72bcd0f0a7f00c13001b65c9463d86765
sarunpat aiyimaphan <maxkubz@hotmail.com>
2023-12-19 20:23:13 +0700

    Add state to login page

--- src/components/login/loginPage.tsx
+++ src/components/login/loginPage.tsx
@@ -1,4 +1,16 @@
+"use client"
+import { useState,ChangeEvent } from "react";
+
 export default function LoginPanel() {
+
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    const handleUsernameChange = (event : ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+    const handlePasswordChange = (event : ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
     return (
         <main>
             <div className="w-[25vw] h-[50vh]  text-black shadow-md flex flex-col items-center ">
@@ -9,6 +21,10 @@ export default function LoginPanel() {
                     focus:border-b-2 focus:border-rose-500 focus:bg-white 
                     transition duration-300 ease-out focus:outline-none"
                         id="username"
+                        value={username}
+                        onChange={(e)=>{
+                            handleUsernameChange(e);
+                        }}
                     >
 
                     </input>
@@ -18,7 +34,12 @@ export default function LoginPanel() {
                     <input className="w-64 h-12 inline px-2 bg-gray-100 rounded-sm
                     focus:border-b-2 focus:border-rose-500 focus:bg-white 
                     transition duration-300 ease-out focus:outline-none"
+                        type="password"
                         id="Password"
+                        value={password}
+                        onChange={(e)=>{
+                            handlePasswordChange(e);
+                        }}
                     >
                     </input>
 
@@ -29,7 +50,11 @@ export default function LoginPanel() {
                 </div>
                 <div className="px-20 pt-8 pb-2">
                     <button className="bg-black w-[100px] h-[50px] rounded-md outline-none bg-rose-400 text-white  
-                    hover:border-2 hover:bg-white hover:border-rose-400 hover:text-rose-400 transition-colors">
+                    hover:border-2 hover:bg-white hover:border-rose-400 hover:text-rose-400 transition-colors"
+                    onClick={()=>{
+                        // get username and password
+                    }}
+                    >
                         Log-in
                     </button>
                 </div>
