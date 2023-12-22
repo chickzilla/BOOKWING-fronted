eab4070f7a078e8c55a745bd30c82bccfb347528
sarunpat aiyimaphan <maxkubz@hotmail.com>
2023-12-22 20:07:10 +0700

    Update page.tsx

--- src/app/runningevent/[eventid]/page.tsx
+++ src/app/runningevent/[eventid]/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import OrganizeDetail from "@/components/detailPage/organizeDetail";
 import RunningDetail from "@/components/detailPage/runningDetail";
 import PackagePanel from "@/components/detailPage/packagePanel";
-import RouteMap from "@/components/detailPage/routeMap";
 import IncludeInEntry from "@/components/detailPage/include";
 
 export default function EventDatailPage({
@@ -13,13 +12,18 @@ export default function EventDatailPage({
   params: { eventid: string };
 }) {
   return (
+    // <main className="h-[300vh] w-[100vw] bg-white mt-[70px]">
+    //   <div className="flex flex-col w-[100vw] h-[200vh] items-center space-y-16 pt-10 ">
+    //     <RunningDetail eventName="Event Name" date="20/20/20" />
+
     <main className="h-[400vh] w-[100vw] bg-white mt-[70px]">
       <div className="flex flex-col w-[100vw] h-[400vh] items-center space-y-14 pt-10 ">
+        
         <RunningDetail eventName="Event Name" date="20/20/20" />
         <OrganizeDetail />
+
         <PackagePanel />
-        <RouteMap imgSrc="/img/route_map.jpg" />
-        <IncludeInEntry />
+        <IncludeInEntry/>
       </div>
     </main>
   );
