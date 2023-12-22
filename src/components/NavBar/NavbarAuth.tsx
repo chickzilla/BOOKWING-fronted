56597287b2fc4241cbc95154f7e9b1744a3d6ad3
sarunpat aiyimaphan <maxkubz@hotmail.com>
2023-12-22 20:24:03 +0700

    nav button again

--- src/components/NavBar/NavbarAuth.tsx
+++ src/components/NavBar/NavbarAuth.tsx
@@ -7,8 +7,8 @@ export default function NavbarAuth() {
   //mock session
   const [session, setSession] = useState(false);
   return (
-    <div className="h-full w-[100%] flex flex-row items-center text-white font-md">
-      <div className="w-[50%] h-[100%] flex items-center justify-center">
+    <div className="h-full w-[100%] flex flex-row items-center text-white font-md justify-end">
+      <div className=" h-[100%] flex items-center justify-center">
         <Link
           href="/register"
           className="border border-white p-2 hover:bg-white hover:text-[#B22222] transition duration-300 rounded-md"
@@ -16,7 +16,7 @@ export default function NavbarAuth() {
           Create account
         </Link>
       </div>
-      <div className="w-[50%] text-center">
+      <div className="h-[100%] flex items-center justify-center pr-8 pl-5">
         {session ? (
           <Link href="/login" className="border border-white p-2 hover:bg-white hover:text-[#B22222] transition duration-300 rounded-md"> Sign out </Link>
         ) : (
