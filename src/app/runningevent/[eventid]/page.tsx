5c8d386ac09e10f30e80feba00f6424065f59039
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-12-22 17:16:20 +0700

    fix conflic

--- src/app/runningevent/[eventid]/page.tsx
+++ src/app/runningevent/[eventid]/page.tsx
@@ -1,25 +1,11 @@
 "use client";
-<<<<<<< Updated upstream
 import React from "react";
 import Image from "next/image";
 import OrganizeDetail from "@/components/detailPage/organizeDetail";
 import RunningDetail from "@/components/detailPage/runningDetail";
 import PackagePanel from "@/components/detailPage/packagePanel";
-||||||| Stash base
-import React from 'react';
-import Image from 'next/image'
-import OrganizeDetail from '@/components/detailPage/organizeDetail';
-import RunningDetail from '@/components/detailPage/runningDetail';
-import PackagePanel from '@/components/detailPage/packagePanel';
-=======
-import React from 'react';
-import Image from 'next/image'
-import OrganizeDetail from '@/components/detailPage/organizeDetail';
-import RunningDetail from '@/components/detailPage/runningDetail';
-import PackagePanel from '@/components/detailPage/packagePanel';
-import RouteMap from '@/components/detailPage/routeMap';
-import IncludeInEntry from '@/components/detailPage/include';
->>>>>>> Stashed changes
+import RouteMap from "@/components/detailPage/routeMap";
+import IncludeInEntry from "@/components/detailPage/include";
 
 export default function EventDatailPage({
   params,
@@ -27,37 +13,13 @@ export default function EventDatailPage({
   params: { eventid: string };
 }) {
   return (
-<<<<<<< Updated upstream
-    <main className="h-[300vh] w-[100vw] bg-white mt-[70px]">
-      <div className="flex flex-col w-[100vw] h-[200vh] items-center space-y-16 pt-10 ">
+    <main className="h-[400vh] w-[100vw] bg-white mt-[70px]">
+      <div className="flex flex-col w-[100vw] h-[400vh] items-center space-y-14 pt-10 ">
         <RunningDetail eventName="Event Name" date="20/20/20" />
-
-||||||| Stash base
-    <main className='h-[300vh] w-[100vw] bg-white mt-[70px]'>
-      <div className='flex flex-col w-[100vw] h-[200vh] items-center space-y-16 pt-10 '>
-        
-        <RunningDetail eventName='Event Name' date='20/20/20' />
-        
-
-=======
-    <main className='h-[400vh] w-[100vw] bg-white mt-[70px]'>
-      <div className='flex flex-col w-[100vw] h-[400vh] items-center space-y-14 pt-10 '>
-        
-        <RunningDetail eventName='Event Name' date='20/20/20' />
->>>>>>> Stashed changes
         <OrganizeDetail />
-<<<<<<< Updated upstream
-
         <PackagePanel />
-||||||| Stash base
-
-        <PackagePanel/>
-
-=======
-        <PackagePanel/>
-        <RouteMap imgSrc = "/img/route_map.jpg"/>
-        <IncludeInEntry/>
->>>>>>> Stashed changes
+        <RouteMap imgSrc="/img/route_map.jpg" />
+        <IncludeInEntry />
       </div>
     </main>
   );
