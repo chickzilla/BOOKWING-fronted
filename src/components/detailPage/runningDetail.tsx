162bb8be62442cf442493421775fa42186dcd93c
sarunpat aiyimaphan <maxkubz@hotmail.com>
2023-12-24 22:16:04 +0700

    Update runningDetail.tsx

--- src/components/detailPage/runningDetail.tsx
+++ src/components/detailPage/runningDetail.tsx
@@ -1,47 +1,38 @@
+'use client'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Image from 'next/image';
-export default function RunningDetail({eventName ,date} : {eventName :string,date:string}) {
+import { useState } from 'react';
+export default function RunningDetail({ eventName, date , imageList }: { eventName: string, date: string ,imageList:Array<string>}) {
+
     return (
-        <div className='flex flex-row w-[80vw] h-[60vh] p-30 overflow-hidden rounded-lg'>
+        <div className='flex flex-row w-[80vw] h-[60vh] p-30 overflow-hidden '>
             <div className='w-[40%] h-[100%] overflow-hidden'>
-                <Image 
+                <Image className='object-cover w-full h-full '
                     src='/img/head_picture.jpg'
                     alt="cover"
-                    width={0}
-                    height={0}
-                    style ={{
-                        width :1000,
-                        height :1000
-                    }}
+                    width={100}
+                    height={100}
                     sizes="500px"
-                   
-
-                />  
+                />
             </div>
             <div className='w-[60%] h-[100%]' >
-                <h1 className='text-5xl pt-10 pb-3 px-8 font-bold text-black'>
-                    Event Name
-                </h1>
-                <p className='text-2xl pt-5 pb-2 px-8 font-normal text-gray-900'>
-                    <LocationOnIcon sx={{display : 'inline'}}/> <p className='pl-6 inline'>rayong ranong yara</p>
-                </p>
-                <p className='text-2xl pt-1 pb-2 px-8 font-normal text-gray-900'>
-                    <CalendarMonthIcon sx={{display : 'inline'}}/> <p className='pl-6 inline'>20/20/20</p>
-                </p>
-                <p className=' text-2xl pt-1 pb-3 px-8 font-normal text-gray-900'>
-                    <AccessTimeIcon sx={{display : 'inline'}}/> <p className='pl-6 inline'>18 :00</p>
-                </p>
-                <p className='text-xl pt-1 pb-3 px-8 font-normal text-gray-900'>
-                    ----------------------------------------------------------------------------
-                </p>
-                <p className='text-2xl pt-1 pb-3 px-8 font-normal text-gray-900'>Distance : 3km 5km 10km</p>
-                <p className='text-xl pt-1 pb-3 px-8 font-normal text-gray-900'>
-                    ----------------------------------------------------------------------------
-                </p>
-
-                
+                <div className='px-8 font-normal text-gray-900'>
+                    <h1 className='text-5xl pt-10 pb-3 font-bold text-black'>Event Name</h1>
+                    <p className='text-2xl pt-5 pb-2'>
+                        <LocationOnIcon sx={{ display: 'inline' }} /> <p className='pl-6 inline'>rayong ranong yara</p>
+                    </p>
+                    <p className='text-2xl pt-1 pb-2'>
+                        <CalendarMonthIcon sx={{ display: 'inline' }} /> <p className='pl-6 inline'>20/20/20</p>
+                    </p>
+                    <p className=' text-2xl pt-1 pb-3'>
+                        <AccessTimeIcon sx={{ display: 'inline' }} /> <p className='pl-6 inline'>18 :00</p>
+                    </p>
+                    <p className='text-xl pt-1 pb-3'>----------------------------------------------------------------------------</p>
+                    <p className='text-2xl pt-1 pb-3'>Distance : 3km 5km 10km</p>
+                    <p className='text-xl pt-1 pb-3'>----------------------------------------------------------------------------</p>
+                </div>
             </div>
 
         </div>
