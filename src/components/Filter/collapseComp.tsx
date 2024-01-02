5ed4681dec0d55f41d9c272bd6dc7037d8b85bb7
sarunpat aiyimaphan <maxkubz@hotmail.com>
2024-01-02 21:53:13 +0700

    Update collapseComp.tsx

--- src/components/Filter/collapseComp.tsx
+++ src/components/Filter/collapseComp.tsx
@@ -3,9 +3,9 @@
 export default function CollapseComp({filterName} : {filterName:string}) {
     return (
         <div className="relative w-full overflow-hidden mt-1 rounded-md ">
-            <input type="checkbox" className="opacity-0 peer z-10 absolute h-12 w-12 right-0" />
-            <div className="bg-gray-100 h-12 w-full pl-5 flex items-center">
-                <h1 className="text-gray-500">
+            <input type="checkbox" className="peer opacity-0 z-10 absolute h-12 w-12 right-0" />
+            <div className="bg-gray-200 h-12 w-full pl-5 flex items-center">
+                <h1 className="text-black">
                     {filterName}
                 </h1>
             </div>
@@ -14,12 +14,28 @@ export default function CollapseComp({filterName} : {filterName:string}) {
                     <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
                 </svg>
             </div>
-            <div className="bg-black text-white overflow-hidden max-h-0 peer-checked:max-h-80 
+            <div className="bg-gray-100 text-gray-500 overflow-hidden max-h-0 peer-checked:max-h-80 
             transition-tranfrom duration-500">
-                <div className="p-4">
-                    <p>hfd</p>
-                    <p>hfd</p>
+                <div className="p-1">
+                    <input className = "m-1" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                    <label htmlFor="vehicle1"> Bangkok</label><br></br>
                 </div>
+                <div className="p-1">
+                    <input className = "m-1" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                    <label htmlFor="vehicle1"> Suohan</label><br></br>
+                </div>
+                <div className="p-1">
+                    <input className = "m-1" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                    <label htmlFor="vehicle1"> Rayong</label><br></br>
+                </div>
+                <div className="p-1">
+                    <input className = "m-1" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                    <label htmlFor="vehicle1"> ChiangMai</label><br></br>
+                </div>
+                {/* <div className="p-4">
+                    <p>hfd</p>
+                    <p>hfd</p>
+                </div> */}
             </div>
 
         </div>
