d9052eae61eafe87cfecffbc9f0711b9606628bd
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-01-05 22:00:48 +0700

    fix login ja

--- src/components/login/loginPage.tsx
+++ src/components/login/loginPage.tsx
@@ -1,72 +1,79 @@
-"use client"
-import { useState,ChangeEvent } from "react";
+"use client";
+import { useState, ChangeEvent } from "react";
 
 export default function LoginPanel() {
-
-    const [username,setUsername] = useState('')
-    const [password,setPassword] = useState('')
-    const handleUsernameChange = (event : ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
-    };
-    const handlePasswordChange = (event : ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
-    return (
-        
-            <div className="w-[25vw] h-[50vh] text-black shadow-md flex flex-col items-center ">
-                <div className="pt-12 cursor-default"> <h1 className="text-5xl">Login</h1> </div>
-                <div className="px-20 pt-8 pb-4">
-                    <label htmlFor="Username" className="block text-sm font-light px-2 py-1">Username</label>
-                    <input className="w-64 h-12 inline px-2 bg-gray-100 rounded-sm
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  return (
+    <div className="w-[25vw] h-[50vh] text-black shadow-md flex flex-col items-center bg-white">
+      <div className="pt-12 cursor-default">
+        {" "}
+        <h1 className="text-5xl">Login</h1>{" "}
+      </div>
+      <div className="px-20 pt-8 pb-4">
+        <label
+          htmlFor="Username"
+          className="block text-sm font-light px-2 py-1"
+        >
+          Username
+        </label>
+        <input
+          className="w-64 h-12 inline px-2 bg-gray-100 rounded-sm
                     focus:border-b-2 focus:border-rose-500 focus:bg-white 
                     transition duration-300 ease-out focus:outline-none"
-                        id="username"
-                        value={username}
-                        onChange={(e)=>{
-                            handleUsernameChange(e);
-                        }}
-                    >
-
-                    </input>
-                </div>
-                <div className="px-20">
-                    <label htmlFor="Password" className="block text-sm font-light px-2 py-1">Password</label>
-                    <input className="w-64 h-12 inline px-2 bg-gray-100 rounded-sm
+          id="username"
+          value={username}
+          onChange={(e) => {
+            handleUsernameChange(e);
+          }}
+        ></input>
+      </div>
+      <div className="px-20">
+        <label
+          htmlFor="Password"
+          className="block text-sm font-light px-2 py-1"
+        >
+          Password
+        </label>
+        <input
+          className="w-64 h-12 inline px-2 bg-gray-100 rounded-sm
                     focus:border-b-2 focus:border-rose-500 focus:bg-white 
                     transition duration-300 ease-out focus:outline-none"
-                        type="password"
-                        id="Password"
-                        value={password}
-                        onChange={(e)=>{
-                            handlePasswordChange(e);
-                        }}
-                    >
-                    </input>
-
-                </div>
-                <div className="w-64 ">
-                    {/* use link instead */}
-                    {/* <p className=" pt-2 text-sm text-gray-500 underline ">Forgot Password?</p> */}
-                </div>
-                <div className="px-20 pt-8 pb-2">
-                    <button className="bg-black w-[100px] h-[50px] rounded-md outline-none bg-rose-400 text-white  
+          type="password"
+          id="Password"
+          value={password}
+          onChange={(e) => {
+            handlePasswordChange(e);
+          }}
+        ></input>
+      </div>
+      <div className="w-64 ">
+        {/* use link instead */}
+        {/* <p className=" pt-2 text-sm text-gray-500 underline ">Forgot Password?</p> */}
+      </div>
+      <div className="px-20 pt-8 pb-2">
+        <button
+          className="bg-black w-[100px] h-[50px] rounded-md outline-none bg-rose-400 text-white  
                     hover:border-2 hover:bg-white hover:border-rose-400 hover:text-rose-400 transition-colors"
-                    onClick={()=>{
-                        // get username and password
-                    }}
-                    >
-                        Log-in
-                    </button>
-                </div>
-                <div className="w-64 text-center">
-                    {/* use link instead */}
-                    {/* <p className=" pt-2 text-sm text-gray-500 inline">
+          onClick={() => {
+            // get username and password
+          }}
+        >
+          Log-in
+        </button>
+      </div>
+      <div className="w-64 text-center">
+        {/* use link instead */}
+        {/* <p className=" pt-2 text-sm text-gray-500 inline">
                         Havn't got account yet? <p className="text-sm text-indigo-500 underline inline">Sign-up</p>
                     </p> */}
-                </div>
-            </div>
-
-
-        
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/register/RegisterPanel.tsx
+++ src/components/register/RegisterPanel.tsx
@@ -1,128 +1,175 @@
-"use client"
+"use client";
 import { useState, ChangeEvent } from "react";
-
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPanel() {
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [email, setEmail] = useState('')
-    const [password2, setPassword2] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
-    };
-    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
-    const handleFirstnameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setFirstname(event.target.value);
-    };
-    const handleLastnameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setLastname(event.target.value);
-    };
-    const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value);
-    };
-    const handlePassword2Change = (event: ChangeEvent<HTMLInputElement>) => {
-        setPassword2(event.target.value);
-    };
-    return (
-        <div className="w-[25vw] h-[80vh] bg-white flex flex-col items-center text-black rounded-md shadow-md ">
-            <h1 className="text-black text-5xl font-bold mt-16">Register</h1>
-            <div className="mt-8 w-[60%] h-[7%]">
-                <label htmlFor="Firstname" className="block text-sm font-light px-2 py-1 ">Fistname</label>
-                <input className="w-full h-full inline px-2 bg-gray-100 rounded-sm
-                    focus:border-b-2 focus:border-rose-500 focus:bg-white 
-                    transition duration-300 ease-out focus:outline-none"
-                    id="Firstname"
-                    required
-                    value={firstname}
-                    onChange={(e) => {
-                        handleFirstnameChange(e);
-                    }}
-                >
-                </input>
-            </div>
-            <div className="mt-8 w-[60%] h-[7%]">
-                <label htmlFor="Lastname" className="block text-sm font-light px-2 py-1 ">Lastname</label>
-                <input className="w-full h-full inline px-2 bg-gray-100 rounded-sm
-                    focus:border-b-2 focus:border-rose-500 focus:bg-white 
-                    transition duration-300 ease-out focus:outline-none"
-                    id="Lastname"
-                    required
-                    value={lastname}
-                    onChange={(e) => {
-                        handleLastnameChange(e);
-                    }}
-                >
-                </input>
-            </div>
-            <div className="mt-8 w-[60%] h-[7%]">
-                <label htmlFor="Email" className="block text-sm font-light px-2 py-1 ">Email</label>
-                <input className="w-full h-full inline px-2 bg-gray-100 rounded-sm
-                    focus:border-b-2 focus:border-rose-500 focus:bg-white 
-                    transition duration-300 ease-out focus:outline-none"
-                    id="Email"
-                    required
-                    value={email}
-                    onChange={(e) => {
-                        handleEmailChange(e);
-                    }}
-                >
-                </input>
-            </div>
-            <div className="mt-8 w-[60%] h-[7%]">
-                <label htmlFor="Username" className="block text-sm font-light px-2 py-1 ">Username</label>
-                <input className="w-full h-full inline px-2 bg-gray-100 rounded-sm
-                    focus:border-b-2 focus:border-rose-500 focus:bg-white 
-                    transition duration-300 ease-out focus:outline-none"
-                    id="username"
-                    required
-                    value={username}
-                    onChange={(e) => {
-                        handleUsernameChange(e);
-                    }}
-                >
+  const router = useRouter();
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const handleFirstnameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFirstname(event.target.value);
+  };
+  const handleLastnameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLastname(event.target.value);
+  };
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handlePassword2Change = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword2(event.target.value);
+  };
+  return (
+    <div className="w-[30vw] h-[80vh] bg-white flex flex-col items-center text-black rounded-md shadow-xl py-5 justify-between">
+      <div className="text-black text-2xl ">Create an accout</div>
+      <TextField
+        id="outlined-basic"
+        label="Firstname"
+        variant="outlined"
+        required
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        value={firstname}
+        onChange={(e) => setFirstname(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Lastname"
+        variant="outlined"
+        required
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        value={lastname}
+        onChange={(e) => setLastname(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <EmailIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
 
-                </input>
-            </div>
-            <div className="mt-8 w-[60%] h-[7%] ">
-                <label htmlFor="Password" className="block text-sm font-light px-2 py-1 ">Password</label>
-                <input className="w-full h-full inline px-2 bg-gray-100 rounded-sm
-                    focus:border-b-2 focus:border-rose-500 focus:bg-white 
-                    transition duration-300 ease-out focus:outline-none"
-                    type="password"
-                    id="Password"
-                    required
-                    value={password}
-                    onChange={(e) => {
-                        handlePasswordChange(e);
-                    }}
-                >
-                </input>
-            </div>
-            <div className="mt-8 w-[60%] h-[7%]">
-                <label htmlFor="Password2" className="block text-sm font-light px-2 py-1 ">Confirm Password</label>
-                <input className="w-full h-full inline px-2 bg-gray-100 rounded-sm
-                    focus:border-b-2 focus:border-rose-500 focus:bg-white 
-                    transition duration-300 ease-out focus:outline-none"
-                    type="password"
-                    id="Password2"
-                    required
-                    value={password2}
-                    onChange={(e) => {
-                        handlePassword2Change(e);
-                    }}
-                >
-                </input>
-
-            </div>
-
-            <button className="mt-10 py-3 px-8 text-white border-2 outline-none border-white bg-rose-400 rounded-md hover:border-2 hover:bg-white hover:border-rose-400 hover:text-rose-400 transition-colors">
-                Register
-            </button>
-
+      <TextField
+        id="outlined-basic"
+        label="Username"
+        variant="outlined"
+        required
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        sx={{
+          width: "90%",
+        }}
+        type="password"
+        size="small"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LockIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Confirm Password"
+        variant="outlined"
+        type="password"
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        required
+        value={password2}
+        onChange={(e) => setPassword2(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LockIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <div className="w-[100%] flex flex-col text-center items-center">
+        <div className="text-center w-[90%] mb-5">
+          <button className="border-2 border-rose-400 bg-rose-400 text-white py-2 px-3 rounded z-30 w-[100%] font-semibold  hover:text-rose-400 transition-colors hover:bg-white">
+            Register
+          </button>
         </div>
-    );
+        <div className="text-center w-[90%] border-t-2 pt-5">
+          <button
+            className="border border-black border-2 text-black py-2 px-3 rounded z-30 w-[100%] hover:bg-slate-700 hover:text-white transition-colors"
+            onClick={() => {
+              router.push("/login");
+            }}
+          >
+            Already have an account? Sign in
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
