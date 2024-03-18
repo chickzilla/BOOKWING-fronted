c57b551a13cbf3c14f4467b962e637fc05a0fbc1
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-03-18 23:38:03 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import CreateHome from "@/components/CreateEvent/CreateHome";
 export default function Home() {
   return (
     <>
-      <main className="bg-neutral-100 dark:bg-gray-900 w-full h-[400vh] flex flex-col items-center pt-[70px]">
+      <main className="bg-neutral-100 dark:bg-gray-900 w-full flex flex-col items-center pt-[70px]">
         <HomeHeader />
         <HomeCardWithText />
         <MapHome />
